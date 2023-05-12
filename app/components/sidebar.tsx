0f964cb670714130fab5b106fcaa0ae839b823c0
wagjie <jie.wag@gmail.com>
2023-05-12 12:02:09 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT杰哥专用</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          只是身边朋友在使用，请勿外传，
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
